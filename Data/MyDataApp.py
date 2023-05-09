8b136ab58654b41725169684ec0b48d48195546e
DoktorGlaze <98591989+DoktorGlaze@users.noreply.github.com>
2023-05-09 17:45:45 +0200

    t

--- Data/MyDataApp.py
+++ Data/MyDataApp.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+from data_methods import bad_word_count
 
 
 
@@ -7,7 +8,9 @@ import pandas as pd
 syftestext = 'et pharetra pharetra massa massa ultricies mi quis hendrerit dolor magna eget est lorem ipsum dolor sit amet consectetur adipiscing elit pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas integer eget aliquet nibh praesent tristique magna sit amet purus gravida quis blandit turpis cursus in hac habitasse platea dictumst quisque sagittis purus sit amet volutpat consequat mauris nunc congue nisi vitae suscipit tellus mauris a diam maecenas sed enim ut sem viverra aliquet eget sit amet tellus cras adipiscing enim eu turpis egestas pretium aenean pharetra magna ac placerat vestibulum lectus mauris ultrices eros in cursus turpis massa tincidunt dui ut ornare lectus sit amet est placerat in egestas erat imperdiet sed euismod nisi porta lorem mollis aliquam ut porttitor leo a diam sollicitudin tempor id eu nisl nunc mi ipsum faucibus vitae aliquet nec ullamcorper sit amet risus nullam eget felis eget nunc lobortis mattis aliquam faucibus purus in massa tempor nec feugiat nisl pretium fusce id velit ut tortor pretium viverra suspendisse potenti nullam ac tortor vitae purus faucibus ornare suspendisse sed nisi lacus sed viverra tellus in hac habitasse platea dictumst vestibulum rhoncus est pellentesque elit ullamcorper dignissim cras tincidunt lobortis feugiat'
 
 # Load data
-df = pd.read_json('Data/Testfil_samtliga.json')
+df = pd.read_json('Data/Testfil_FINAL.json')
+
+
 
 
 # Title
@@ -23,8 +26,8 @@ with col1:
 with col2:
     st.header('Total inom IT')
     st.header('Dåliga ord:')
-    # Extract the year from the date column and convert it to an integer
-    df['publication_date'] = df['publication_date'].str[:4].astype(int)
+    bad_words = bad_word_count()
+    st.table(bad_words)
 
     # Slider för år
     min_value = df['publication_date'].min()
@@ -36,6 +39,10 @@ with col2:
     # Selectbox för yrkesroll
     occupation_group_list = df['occupation_group.label'].unique().tolist()
     option = st.selectbox('Välj yrkesroll:', occupation_group_list)
+    
+
+
+    
 
 
 st.header('AI analys')
