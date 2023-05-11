d17996ac59b018b631ca3e9deb5261e1949d3ecc
DoktorGlaze <98591989+DoktorGlaze@users.noreply.github.com>
2023-05-11 12:57:56 +0200

    fixat slider och selectbox för filtrering av data

--- Data/MyDataApp.py
+++ Data/MyDataApp.py
@@ -18,35 +18,68 @@ with open('Data/Testfil_FINAL.json', 'r', encoding='utf-8') as f:
 #with open('Data/Testfil_FINAL.json', 'r', encoding='utf-8') as f:
 #    job_ads = json.load(f, object_hook=lambda d: {k: v for k, v in d.items() if k in ('description.text', 'occupation_group.label', 'Bad_words')})
 
+# Kod för att gömma index kolumnen i tables. Fungerar ej för dataframes i senaste streamlit version
+# CSS to inject contained in a string
+hide_table_row_index = """
+            <style>
+            thead tr th:first-child {display:none}
+            tbody th {display:none}
+            </style>
+            """
+
+# Inject CSS with Markdown
+st.markdown(hide_table_row_index, unsafe_allow_html=True)
+
+##############################
 
 # Title
 st.title('Titeltext',)
 
 # Kolumner 
-col1, col2 = st.columns([2,1])
+col1, col2 = st.columns([1,1])
 with col1:
     st.header('Syfte')
     # Syftestext
     st.markdown(f'<span style="word-wrap:break-word;">{syftestext}</span>', unsafe_allow_html=True)
 
 with col2:
-        # Slider för år
+
+    # Interatkivitet
+    # Slider för år
     min_value = df['publication_date'].min()
     max_value = df['publication_date'].max()
 
-    variable_1 = st.slider('Välj år', min_value=int(min_value), max_value=int(max_value), value=2017)
-    st.write('Valt år:', variable_1)
+    year_interval = st.slider('Välj år', min_value=int(min_value), max_value=int(max_value), value=(2016, 2023))
+    st.write('Vald tidsintervall:', year_interval[0],'-',year_interval[1])
 
     # Selectbox för yrkesroll
     occupation_group_list = df['occupation_group.label'].unique().tolist()
-    option = st.selectbox('Välj yrkesroll:', occupation_group_list)
+    occupation_group_list.insert(0, 'Alla')
+    occupation_group = st.selectbox('Välj yrkesroll:', occupation_group_list, )
+
+    # Konverterar valet av yrkesroll till en lista för att fungera med filtret
+    if occupation_group == 'Alla':
+        occupation_group = occupation_group_list
+    else:
+        occupation_group = [occupation_group]
+
+    # Filter appliceras innan datan skickas in i metoder
+    filter = (df['publication_date'] >= year_interval[0]) & (df['publication_date'] <= year_interval[1]) & (df['occupation_group.label'].isin(occupation_group))
+
+    job_ads = df[filter]
+    ##############################
+    
+    # Sektion för Total inom IT
     st.header('Total inom IT')
     bad_ads = bad_ads_and_words(job_ads)
     st.table(bad_ads)
+    ##############################
+    
+    # Sektion för dåliga ord
     st.header('Dåliga ord:')
-    bad_words = bad_word_count(option, job_ads)
+    bad_words = bad_word_count(job_ads)
     st.table(bad_words)
-
+    ##############################
 
     
 
